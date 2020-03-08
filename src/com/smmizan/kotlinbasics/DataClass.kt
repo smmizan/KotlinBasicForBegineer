65fa968782e6443b18de25d8092c4b6d142b385b
unknown <smmizancse@gmail.com>
2020-03-08 12:42:47 +0600

    data class details

--- src/com/smmizan/kotlinbasics/DataClass.kt
+++ src/com/smmizan/kotlinbasics/DataClass.kt
@@ -5,26 +5,26 @@ fun main(args:Array<String>){
     val student = Student("Mizan",206)
     println("Name : ${student.Name} , ID : ${student.ID}")
 
-    //use toString Function in Data Class
+    /*use toString Function in Data Class*/
 
     println(student.toString())
 
 
-    // data class Copy Function Example
+    /* data class Copy Function Example*/
 
     val student2 = student.copy("Shakib")
     println("Student Name : ${student.Name} , Student ID : ${student.ID}")
     println("Student Name : ${student2.Name} ,Student ID : ${student2.ID}")
 
 
-    // data class equal function
+    /* data class equal function*/
 
     val student3 = Student("Mizan",206)
     val student4 = Student("Mizan",206)
 
     println(student3.equals(student4))
 
-    // data class access using component properties
+    /* data class access using component properties*/
 
     println("*****access component properties*****")
     println(" Name : ${student.component1()} , ID : ${student.component2()}")
