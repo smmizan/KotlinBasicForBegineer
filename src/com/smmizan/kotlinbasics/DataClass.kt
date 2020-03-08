acd0b04b10ebef3bb44601f41550e3cf131eea92
unknown <smmizancse@gmail.com>
2020-03-08 12:24:06 +0600

    data class copy function

--- src/com/smmizan/kotlinbasics/DataClass.kt
+++ src/com/smmizan/kotlinbasics/DataClass.kt
@@ -5,7 +5,16 @@ fun main(args:Array<String>){
     val student = Student("Mizan",206)
     println("Name : ${student.Name} , ID : ${student.ID}")
 
+    //use toString Function in Data Class
+
     println(student.toString())
+
+
+    // data class Copy Function Example
+
+    val student2 = student.copy("Shakib")
+    println("Student Name : ${student.Name} , Student ID : ${student.ID}")
+    println("Student Name : ${student2.Name} ,Student ID : ${student2.ID}")
 
 }
 
